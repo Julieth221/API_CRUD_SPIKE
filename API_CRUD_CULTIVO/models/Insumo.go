ca741c6cf85cf0cd0ed878aa2561160b376f881c
Julieth221 <darlycuberos17@gmail.com>
2025-06-11 13:12:22 -0500

    cambios actualizados

--- API_CRUD_CULTIVO/models/Insumo.go
+++ API_CRUD_CULTIVO/models/Insumo.go
@@ -62,7 +62,7 @@ func GetInsumoById(id int) (v *Insumo, err error) {
 func GetAllInsumo(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Insumo))
+	qs := o.QueryTable(new(Insumo)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
